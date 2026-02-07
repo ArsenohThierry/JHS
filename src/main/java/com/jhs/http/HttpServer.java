548a180f683e6675c988e7585f31c8135e5bce6f
RATANDRA Thierry Arsenoh <alcraftr@gmail.com>
2026-02-07 09:48:18 +0300

    #

--- src/main/java/com/jhs/http/HttpServer.java
+++ src/main/java/com/jhs/http/HttpServer.java
@@ -1,5 +1,6 @@
 package com.jhs.http;
 
+import com.jhs.http.config.Configuration;
 import com.jhs.http.config.ConfigurationManager;
 
 /*
@@ -8,8 +9,12 @@ import com.jhs.http.config.ConfigurationManager;
 
 public class HttpServer {
     public static void main(String[] args) {
-        System.out.println("Starting HTTP Server...");
-        
+        System.out.println("Demarrage du Serveur HTTP...");
+
         ConfigurationManager.getInstance().loadConfiguration("src/main/resources/http.json");
+        Configuration configuration = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("port " + configuration.getPort());
+        System.out.println("Repertoire racine web: " + configuration.getWebRoot());
     }
 }
