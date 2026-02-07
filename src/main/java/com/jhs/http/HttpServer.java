3d3bcf91e7a4eca1fe73b082e6a2ce8565ee292e
RATANDRA Thierry Arsenoh <alcraftr@gmail.com>
2026-02-07 10:32:57 +0300

    Le serveur ecoute sur le port voulu et peux deja afficher une page web

--- src/main/java/com/jhs/http/HttpServer.java
+++ src/main/java/com/jhs/http/HttpServer.java
@@ -1,5 +1,11 @@
 package com.jhs.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import com.jhs.http.config.Configuration;
 import com.jhs.http.config.ConfigurationManager;
 
@@ -16,5 +22,38 @@ public class HttpServer {
 
         System.out.println("port " + configuration.getPort());
         System.out.println("Repertoire racine web: " + configuration.getWebRoot());
+
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(configuration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream(); 
+
+            // TODO Lecture de la requete HTTP et generation de la reponse HTTP
+
+            //TODO Ecriture de la reponse HTTP dans le outputStream
+
+            String html = "<html><title>Simple http page</title><body><h1>Bonjour, Monde!</h1></body></html>";
+
+            final String CRLF = "\r\n"; //13, 10
+
+            String response = 
+                "HTTP/1.1 200 OK" + CRLF + // ligne de statut 
+                "Content-length: " + html.getBytes().length + CRLF + // entete de la reponse (header)  
+                CRLF + CRLF +
+                html;
+                
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+            
+        } catch (IOException e) {
+            
+        }
     }
 }
