fe1e74b4d5beee8ccf51efeaf3685bceedfde44c
RATANDRA Thierry Arsenoh <alcraftr@gmail.com>
2026-02-07 08:38:16 +0300

    #

---
+++ src/main/java/com/jhs/http/HttpServer.java
@@ -0,0 +1,11 @@
+package com.jhs.http;
+
+/*
+*   Driver pour le serveur HTTP
+*/
+
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Starting HTTP Server...");
+    }
+}
